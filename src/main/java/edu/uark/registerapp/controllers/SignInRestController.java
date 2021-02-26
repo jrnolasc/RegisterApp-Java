e85ddc420a949217e8818835b575176fb8add1e2
kaylasam <kaylasamanthaa@gmail.com>
2021-02-26 00:09:39 -0600

    step 2/ step 3 in progress

--- src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
@@ -2,11 +2,14 @@ package edu.uark.registerapp.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.uark.registerapp.commands.activeUsers.ActiveUserDeleteCommand;
+// import edu.uark.registerapp.commands.activeUsers.ValidateActiveUserCommand;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
 
@@ -19,8 +22,23 @@ public class SignInRestController extends BaseRestController {
 	) {
 
 		// TODO: Sign out the user associated with request.getSession().getId()
+		// return this.validateActiveUserCommand
+		// 	.setSessionKey(request)
+		// 	.execute();
+
+			this.activeUserDeleteCommand.setSessionKey( 
+			request.getSession().getId()).execute();
 
 		return (new ApiResponse())
 			.setRedirectUrl(ViewNames.SIGN_IN.getRoute());
+
 	}
+
+	//Properties
+
+	@Autowired
+	private ActiveUserDeleteCommand activeUserDeleteCommand;
+
+	// @Autowired
+	// private ValidateActiveUserCommand validateActiveUserCommand;
 }
