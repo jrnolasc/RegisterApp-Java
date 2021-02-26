7a7d5b1aefc25fe757573427bac79abdaff3cb7c
kaylasam <kaylasamanthaa@gmail.com>
2021-02-26 00:15:19 -0600

    added a setSessionKey method

--- src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -16,8 +16,17 @@ public class ActiveUserDeleteCommand implements VoidCommandInterface {
     private String sKey; //session key
     //Properties
     //Current session key    
-    public String getSesKey(){
-        return this.sKey;
+    //public String getSesKey(){
+    //    return this.sKey;
+    //}
+
+    public String getSessionKey() {
+		return this.sKey;
+	}
+
+	public ActiveUserDeleteCommand setSessionKey(final String sKey) {
+    	this.sKey = sKey;
+    	return this;
     }
     //In transaction use @Transactional here
         //Query the activeuser table for a record with provided session key
