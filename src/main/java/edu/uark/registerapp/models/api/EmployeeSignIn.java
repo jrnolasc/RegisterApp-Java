194ce54e11d331657b8985441342e9707818d9e4
kaylasam <kaylasamanthaa@gmail.com>
2021-02-26 12:12:18 -0600

    get and set employeeId and password

--- src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
+++ src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
@@ -4,4 +4,21 @@ public class EmployeeSignIn {
     String employeeId;
     String password;
 
+    //get and set employeeId;
+    public String getEmployeeId() {
+    	return this.employeeId;
+    }
+    public EmployeeSignIn setEmployeeId(final String employeeId) {
+		this.employeeId = employeeId;
+		return this;
+	}
+
+    //get and set password;
+    public String getPassword() {
+    	return this.password;
+    }
+    public EmployeeSignIn setPassword(final String password) {
+		this.password = password;
+		return this;
+	}
 }
