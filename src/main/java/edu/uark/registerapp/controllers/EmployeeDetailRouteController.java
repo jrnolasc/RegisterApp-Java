d8d5fda11a73550104e45726e8f29b0e722120bb
kaylasam <kaylasamanthaa@gmail.com>
2021-03-01 01:03:17 -0600

    Update EmployeeDetailRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
@@ -23,42 +23,42 @@ import edu.uark.registerapp.models.entities.ActiveUserEntity;
 @Controller
 @RequestMapping(value = "/employeeDetail")
 public class EmployeeDetailRouteController extends BaseRouteController {
-	//@RequestMapping(method = RequestMethod.GET)
-	//public ModelAndView start(
-	//  	@RequestParam final Map<String, String> queryParameters,
-	//  	final HttpServletRequest request
-	//  ) {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView start(
+	 	@RequestParam final Map<String, String> queryParameters,
+	 	final HttpServletRequest request
+	 ) {
 
-		// TODO: Logic to determine if the user associated with the current session
-		//  is able to create an employee
+		//TODO: Logic to determine if the user associated with the current session
+		// is able to create an employee
 
-		//return new ModelAndView(ViewModelNames.EMPLOYEE_TYPES.getValue());
-	//}
+		return new ModelAndView(ViewModelNames.EMPLOYEE_TYPES.getValue());
+	}
 
-	//@RequestMapping(value = "/{employeeId}", method = RequestMethod.GET)
-	// public ModelAndView startWithEmployee(
-	// 	@PathVariable final UUID employeeId,
-	// 	@RequestParam final Map<String, String> queryParameters,
-	// 	final HttpServletRequest request
-	// ) {
+	@RequestMapping(value = "/{employeeId}", method = RequestMethod.GET)
+	public ModelAndView startWithEmployee(
+		@PathVariable final UUID employeeId,
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
 
-		// final Optional<ActiveUserEntity> activeUserEntity =
-		// 	this.getCurrentUser(request);
+		final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
 
-		// if (!activeUserEntity.isPresent()) {
-		// 	return this.buildInvalidSessionResponse();
-		// } else if (!this.isElevatedUser(activeUserEntity.get())) {
-		// 	return this.buildNoPermissionsResponse();
-		// }
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
+		} else if (!this.isElevatedUser(activeUserEntity.get())) {
+			return this.buildNoPermissionsResponse();
+		}
 
 		// TODO: Query the employee details using the request route parameter
 		// TODO: Serve up the page
-		//return new ModelAndView(ViewModelNames.EMPLOYEE_TYPES.getValue());
-	//}
+		return new ModelAndView(ViewModelNames.EMPLOYEE_TYPES.getValue());
+	}
 
 	// Helper methods
-	// private boolean activeUserExists() {
-	// 	// TODO: Helper method to determine if any active users Exist
-	// 	return true;
-	// }
+	private boolean activeUserExists() {
+		// TODO: Helper method to determine if any active users Exist
+		return true;
+	}
 }
