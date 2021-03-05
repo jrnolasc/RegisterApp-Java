1f7bb42289c115090629623b645c6be449a0d40d
Julio Nolasco <jrnolasc@email.uark.edu>
2021-03-04 20:58:39 -0600

    Another exception change

--- src/main/java/edu/uark/registerapp/commands/exceptions/UnauthorizedException.java
+++ src/main/java/edu/uark/registerapp/commands/exceptions/UnauthorizedException.java
@@ -1,7 +1,7 @@
 package edu.uark.registerapp.commands.exceptions;
 
 public class UnauthorizedException extends RuntimeException {
-	public UnauthorizedException() { super("User is not authorized."); }
+	public UnauthorizedException() { super("Manager Found Proceed With Button Bellow"); }
 
 	private static final long serialVersionUID = 2725536148764655946L;
 }
