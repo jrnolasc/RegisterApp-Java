d6c2be94bb5b08df1221e0cd280956073d5fcaca
Julio Nolasco <jrnolasc@email.uark.edu>
2021-03-04 19:17:03 -0600

    Employee page exception

--- src/main/java/edu/uark/registerapp/commands/exceptions/UnprocessableEntityException.java
+++ src/main/java/edu/uark/registerapp/commands/exceptions/UnprocessableEntityException.java
@@ -2,7 +2,7 @@ package edu.uark.registerapp.commands.exceptions;
 
 public class UnprocessableEntityException extends RuntimeException {
 	public UnprocessableEntityException(String parameterName) {
-		super("Missing or invalid parameter: ".concat(parameterName));
+		super("'Functionality for Employee creation has not yet been implemented.'".concat(parameterName));
 	}
 
 	private static final long serialVersionUID = 6128380142041898414L;
